788ed1ecc01f8c1b224c02adeea4a5440ce49a85
Luthfiahmad12 <luthfiafif625@gmail.com>
2025-05-02 17:38:58 +0700

    fix:add alert in ui

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -9,6 +9,14 @@
 
     <div class="row">
         <div class="col-12">
+            @if (session()->has('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <i class="mdi mdi-check-all me-2"></i>
+                    {{ session('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                    </button>
+                </div>
+            @endif
             <div class="card">
                 <div class="card-body">
                     <div class="d-flex align-items-center justify-content-between mb-4">
